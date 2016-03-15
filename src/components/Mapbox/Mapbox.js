1f06549946c5194d43a569f6c949790497530640
Alex Kerney <abk@mac.com>
2016-03-15 11:20:10 -0400

    Initial geojson getting thrown on map

--- src/components/Mapbox/Mapbox.js
+++ src/components/Mapbox/Mapbox.js
@@ -9,11 +9,21 @@ type Props = {
 export class Mapbox extends React.Component {
   props: Props;
 
+  setGeoJSON () {
+    this.map.featureLayer.setGeoJSON(this.props.geojson)
+  }
+
   componentDidMount () {
     this.map = L.mapbox.map('map', this.props.layer, { //eslint-disable-line
       accessToken: this.props.accessToken
     }).fitBounds(this.props.bounds)
+    this.setGeoJSON()
   }
+
+  componentDidUpdate () {
+    this.setGeoJSON()
+  }
+
 
   render () {
     return (
