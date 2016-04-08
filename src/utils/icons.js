327b08ee04caf1a4c8cda71e77bec0a15d6d4aaa
Alex Kerney <abk@mac.com>
2016-04-08 12:05:41 -0400

    Don't add base url to icons if they are externally hosted

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,6 +1,9 @@
 export const iconUrl = (feature) => {
   let base = 'images/icons/'
   if (feature.properties.icon) {
+    if (feature.properties.icon.includes('http')) {
+      return feature.properties.icon
+    }
     return base + feature.properties.icon
   } else {
     return base + 'maki-svg/marker-24.svg'
