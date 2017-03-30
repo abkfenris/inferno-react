7c94fddfc59cf4f28389847ff0e57bfca9bb410d
Alex Kerney <abk@mac.com>
2017-03-29 21:42:34 -0400

    Forget about dist

--- config/_base.js
+++ config/_base.js
@@ -12,7 +12,7 @@ const config = {
   // ----------------------------------
   path_base  : path.resolve(__dirname, '..'),
   dir_client : 'src',
-  dir_dist   : 'dist',
+  dir_dist   : 'docs',
   dir_server : 'server',
   dir_test   : 'tests',
 
